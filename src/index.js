37c01977732f1120dca45a654948a2bd6a7a9bb0
Misha Marinenko <0x77dev@protonmail.com>
2020-02-16 23:17:03 +0200

    init

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import * as THREE from 'three';
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 10000);
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+const geometry = new THREE.BoxGeometry(700, 700, 700, 10, 10, 10);
+const material = new THREE.MeshBasicMaterial({ color: 0xfffff, wireframe: true });
+const cube = new THREE.Mesh(geometry, material);
+
+scene.add(cube);
+
+camera.position.z = 1000;
+
+const render = () => {
+  requestAnimationFrame(render);
+  cube.rotation.x += 0.01;
+  cube.rotation.y += 0.01;
+  renderer.render(scene, camera);
+};
+
+render();
